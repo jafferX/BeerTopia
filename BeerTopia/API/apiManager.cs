f5c76274387026be0082c9c36d74bb88e719c506
Erik <eriklab1222@gmail.com>
2017-12-02 17:35:24 -0800

    added apiManager, GetBeerByName, and GetSampleBeers

---
+++ BeerTopia/API/apiManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using BeerTopia.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BeerTopia.API
+{
+    class ApiManager
+    {
+        private string apiURL = "http://api.brewerydb.com/v2/";
+
+        HttpClient StartHTTP()
+        {
+            HttpClient client = new HttpClient();
+            return client;
+        }
+
+        async void GetSampleBeers (ObservableCollection<Beer> observableCollection)
+        {
+            HttpClient client = StartHTTP();
+            var uri = new Uri(
+                string.Format(
+                    $"{apiURL}beers?key={ApiKey.BeerKey}&styleId=15"));
+            var response = await client.GetAsync(uri);
+            Beer beerData = null;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                beerData = null;
+            }
+            observableCollection.Add(beerData);
+        }
+
+        async void GetBeerByName(ObservableCollection<Beer> observableCollection, string userInput)
+        {
+            HttpClient client = StartHTTP();
+            var uri = new Uri(
+                string.Format(
+                    $"{apiURL}beers?key={ApiKey.BeerKey}&name={userInput}"));
+            var response = await client.GetAsync(uri);
+            Beer beerData = null;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                beerData = null;
+            }
+            observableCollection.Add(beerData);
+        }
+
+    }
+}
